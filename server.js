09014b42a2b9f0739235a74607ad085adcc81529
Rohit <rohit.krishnan@gmail.com>
2023-12-08 21:12:23 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Endpoint to provide the Google Client ID to the frontend
 app.get('/api/client-id', (req, res) => {
   res.json({ clientId: process.env.CLIENT_ID });
 });
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+app.use(express.static('.')); // Serve static files from the current directory
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
