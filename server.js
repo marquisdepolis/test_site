525ba2742a213f954fd7ec5dab178a981b4e2c92
Rohit <rohit.krishnan@gmail.com>
2023-12-08 21:56:21 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/client-id', (req, res) => {
-  res.json({ clientId: process.env.CLIENT_ID });
-});
-
 app.use(express.static('.')); // Serve static files from the current directory
 
 app.listen(port, () => {
