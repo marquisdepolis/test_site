5326718fa7ad9ef9023118cc31a914e1e5b59616
Rohit <rohit.krishnan@gmail.com>
2023-12-08 23:57:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,28 @@
-let gapiLoadAttempts = 0;
-
 window.onload = function() {
     google.accounts.id.initialize({
-        client_id: '149913241851-j6himeafqd5snvi98gt8ah7fa0meitqj.apps.googleusercontent.com',
+        client_id: '149913241851-j6himeafqd5snvi98gt8ah7fa0meitqj.apps.googleusercontent.com', // Use your actual Client ID
         callback: onSignIn
     });
     google.accounts.id.renderButton(
-        document.getElementById('signin-button'),
+        document.getElementById('signin-button'), // Ensure this element exists in your HTML
         { theme: 'outline', size: 'large' }  // Customization attributes
     );
     google.accounts.id.prompt(); // This will prompt the user to select an account
 };
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('Full Name: ' + profile.getName());
-    console.log('Email: ' + profile.getEmail());
-    console.log('User signed in:', response);
+function onSignIn(response) {
+    // The ID token you need to pass to your backend:
+    let id_token = response.credential;
 
+    // Decode the token to get user info (optional, for frontend use)
+    const user = JSON.parse(atob(id_token.split('.')[1]));
+    console.log('User Email:', user.email);
     // Display user information or a welcome message
     var userInfoDiv = document.getElementById('user-info');
-    userInfoDiv.innerHTML = 'Hello, ' + profile.getName() + '! You are now signed in.';
+    userInfoDiv.innerHTML = 'Hello, ' + user.name + '! You are now signed in.';
     userInfoDiv.style.display = 'block';
 }
 
 function onFailure(error) {
     console.error('Sign-In Error:', error);
 }
-
-function renderButton() {
-    gapi.signin2.render('signin-button', {
-        'scope': 'email profile https://www.googleapis.com/auth/gmail.readonly',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSignIn,
-        'onfailure': onFailure
-    });
-}
-
-function loadGapiAndInit() {
-    if (window.gapi) {
-        gapi.load('auth2', function() {
-            gapi.auth2.init({
-                client_id: '149913241851-j6himeafqd5snvi98gt8ah7fa0meitqj.apps.googleusercontent.com', // Replace with your actual Client ID
-                cookiepolicy: 'single_host_origin',
-            }).then(renderButton, function(error) {
-                // Log the full error object to the console
-                console.error('Error initializing Google Auth:', error);
-                // Attempt to log more detailed error information
-                console.error('Detailed error message:', error.details);
-            });
-        });
-    } else if (gapiLoadAttempts < 3) {
-        gapiLoadAttempts++;
-        setTimeout(loadGapiAndInit, 1000); // Retry after 1 second
-    } else {
-        console.error('Failed to load Google API');
-    }
-}
-
-// Start the process
-loadGapiAndInit();
