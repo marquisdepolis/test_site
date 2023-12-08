4492569671a1d5e65725e0395b7d11e6489281e2
Rohit <rohit.krishnan@gmail.com>
2023-12-08 23:16:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,34 @@ function startApp() {
     });
 }
 
+let gapiLoadAttempts = 0;
+
+function loadGapiAndInit() {
+    if (window.gapi) {
+        gapi.load('auth2', function() {
+            gapi.auth2.init({
+                client_id: 'YOUR_CLIENT_ID',
+                cookiepolicy: 'single_host_origin',
+            }).then(renderButton, function(error) {
+                console.error('Error initializing Google Auth:', error);
+            });
+        });
+    } else if (gapiLoadAttempts < 3) {
+        gapiLoadAttempts++;
+        setTimeout(loadGapiAndInit, 1000); // Retry after 1 second
+    } else {
+        console.error('Failed to load Google API');
+    }
+}
+
+function renderButton() {
+    gapi.signin2.render('signin-button', {
+        // Existing button configuration...
+    });
+}
+
+loadGapiAndInit();
+
 if (window.gapi) {
     startApp();
 } else {
