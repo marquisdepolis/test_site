1a4b712a304a4018da0da68e4377decc1e240b59
Rohit <rohit.krishnan@gmail.com>
2023-12-13 12:36:07 -0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, request, url_for
+import googleapiclient.discovery
+
+app = Flask(__name__)
+
+@app.route("/auth/google/callback")
+def google_callback():
+  id_token = request.form.get("id_token")
+  access_token = request.form.get("access_token")
+
+  # Use the ID token and access token to authenticate the user.
+
+  # If the user is authenticated, create a session for them.
+
+  # Redirect the user to the home page.
+
+  return redirect(url_for("home"))
+
+
+@app.route("/auth/google/store_refresh_token", methods=["POST"])
+def store_refresh_token():
+  refresh_token = request.form.get("refresh_token")
+
+  # Store the refresh token in a database.
+
+  return jsonify({"success": True})
+
+
+def get_new_access_token(refresh_token):
+  """Gets a new access token from a refresh token."""
+
+  # Create a new OAuth2 client.
+  client = googleapiclient.discovery.build(
+      "oauth2", "v2",
+      client_id=os.environ["GOOGLE_CLIENT_ID"],
+      client_secret=os.environ["GOOGLE_CLIENT_SECRET"])
+
+  # Use the refresh token to obtain a new access token.
+  new_access_token, _ = client.tokeninfo(access_token=refresh_token).execute()
+
+  return new_access_token
+
+
+if __name__ == "__main__":
+  app.run()
